05168539643019b1996813c827b4c795b333055b
Igal Kolihman <igal.kolihman@gmail.com>
2022-11-05 17:30:31 +0200

    Added filtering abilities to the sniffer

--- sniffer.py
+++ sniffer.py
@@ -1,11 +1,11 @@
 import configparser
+from typing import Dict, Union
 from collections import ChainMap
 from configparser import SectionProxy
-from typing import Dict, Union
 
 import loguru
-from pymongo import MongoClient
 from scapy.all import sniff
+from pymongo import MongoClient
 
 from databases import IDatabase, init_db
 
@@ -14,60 +14,27 @@ logger = loguru.logger
 client = MongoClient("mongodb://admin:admin@localhost")
 traffic_db = client["traffic"]["Legion"]
 
-_native_value = (int, float, str, bytes, bool, list, tuple, set, dict, type(None))
-
-
-class Packet2Dict:
-    def __init__(self, packet):
-        self.packet = packet
-
-    def to_dict(self):
-        """
-        Turn every layer to dict, store in ChainMap type.
-        :return: ChainMaq
-        """
-        d = list()
-        count = 0
-
-        while True:
-            layer = self.packet.getlayer(count)
-            if not layer:
-                break
-            d.append(self._layer2dict(layer))
-
-            count += 1
-        return dict(ChainMap(*d))
-
-    def _layer2dict(self, obj):
-        layer = {}
-
-        if not getattr(obj, "fields_desc", None):
-            return
-
-        for field in obj.fields_desc:
-            value = getattr(obj, field.name)
-            if value is None:
-                value = None
-
-            if not isinstance(value, _native_value):
-                value = self._layer2dict(value)
-            layer[field.name] = value
-
-        return {obj.name: layer}
-
 
 class Sniffer:
+    NATIVE_VALUES = (int, float, str, bytes, bool, list, tuple, set, dict, type(None))
+
     def __init__(self, database: IDatabase, conf: SectionProxy):
         self.db = database
         self.config = conf
         self.interface = self.config["interface"]
         self.verbosity_level = self.config.get("logging", None)
-        ignored_fields = self.config.get("ignore_fields", None)
 
-        if ignored_fields is not None:
-            ignored_fields = ignored_fields.strip().split("\n")
+        excluded_fields = self.config.get("exclude_layers", None)
+        only_fields = self.config.get("only_layers", None)
 
-        self.ignored_fields = ignored_fields
+        if excluded_fields is not None:
+            excluded_fields = excluded_fields.strip().split("\n")
+
+        if only_fields is not None:
+            only_fields = only_fields.strip().split("\n")
+
+        self.excluded_fields = excluded_fields if excluded_fields else []
+        self.only_fields = only_fields if only_fields else []
 
     def sniff(self):
         logger.info(f"Beginning sniff for interface {self.interface}")
@@ -98,15 +65,18 @@ class Sniffer:
         Turn every layer to dict, store in ChainMap type.
         """
         d = list()
-        count = 0
 
-        while True:
-            layer = packet.getlayer(count)
-            if not layer:
-                break
+        for i in range(len(packet.layers())):
+            layer = packet.getlayer(i)
 
-            d.append(self._parse_layer(layer))
-            count += 1
+            if layer.name in self.excluded_fields:
+                continue
+
+            if not self.only_fields:
+                d.append(self._parse_layer(layer))
+
+            elif layer.name in self.only_fields:
+                d.append(self._parse_layer(layer))
 
         return dict(ChainMap(*d))
 
@@ -121,8 +91,9 @@ class Sniffer:
             if value is None:
                 value = None
 
-            if not isinstance(value, _native_value):
+            if not isinstance(value, self.NATIVE_VALUES):
                 value = self._parse_layer(value)
+
             layer[field.name] = value
 
         return {obj.name: layer}
@@ -132,10 +103,6 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read("conf.ini")
     db_config, sniff_config = config["DATABASE"], config["SNIFFER"]
-    import ipdb;
-
-    ipdb.set_trace(context=20)
-    exit()
 
     db = init_db(config=db_config, interface=sniff_config["interface"])
     sniffer = Sniffer(db, sniff_config)
